deb6b6a5f24a1de0d853b1d690ae5433edcf99f7
amart251 <amartinez251@miners.utep.edu>
2023-07-20 13:44:35 -0600

    ballin format

--- lcdLib/lcddemo.c
+++ lcdLib/lcddemo.c
@@ -16,13 +16,13 @@ main()
 
   clearScreen(COLOR_BLUE);
 
-  drawString5x7(20,20, "I put the new Forgis on the Jeep
-I trap until the, bloody bottoms is underneath
-'Cause all my niggas got it out the streets
+  drawString5x7(80,80, "I put the new Forgis on the Jeep
+I trap until the bloody bottoms is underneath
+Cause all my niggas got it out the streets
 I keep a hundred racks inside my jeans
-I remember hittin' the mall with the whole team
-Now a nigga can't answer calls 'cause I'm ballin'
-I was wakin' up gettin' racks in the mornin'", COLOR_GREEN, COLOR_RED);
+I remember hittin the mall with the whole team
+Now a nigga cant answer calls cause Im ballin
+I was wakin up gettin racks in the mornin", COLOR_GREEN, COLOR_RED);
 
   fillRectangle(30,30, 60, 60, COLOR_ORANGE);
   
