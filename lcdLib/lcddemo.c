399cdb5a8611bfb858b92a4c8286f522fe050e52
amart251 <amartinez251@miners.utep.edu>
2023-07-20 14:00:49 -0600

    ballin test

--- lcdLib/lcddemo.c
+++ lcdLib/lcddemo.c
@@ -16,9 +16,14 @@ main()
 
   clearScreen(COLOR_BLUE);
 
-  drawString5x7(0,20, "https://github.com/u", COLOR_GREEN, COLOR_RED);
-  drawString5x7(0,30, "tep-cs-systems-courses", COLOR_GREEN, COLOR_RED);
-  drawString5x7(0,40, "/p3-lcd-amart251.git", COLOR_GREEN, COLOR_RED);
+  drawString5x7(0, 00, "I put the new Forgis", COLOR_GREEN, COLOR_RED);
+  drawString5x7(0, 10, "on the Jeep", COLOR_GREEN, COLOR_RED);
+  drawString5x7(0, 20, "I trap until the,", COLOR_GREEN, COLOR_RED);
+  drawString5x7(0, 30, "bloody bottoms is", COLOR_GREEN, COLOR_RED);
+  drawString5x7(0, 40, "underneath", COLOR_GREEN, COLOR_RED);
+  drawString5x7(0, 50, "'Cause all my nigga", COLOR_GREEN, COLOR_RED);
+  drawString5x7(0, 60, "s got it out the st", COLOR_GREEN, COLOR_RED);
+  drawString5x7(0, 70, "reets", COLOR_GREEN, COLOR_RED);
 
   //fillRectangle(30,30, 60, 60, COLOR_ORANGE);
   
