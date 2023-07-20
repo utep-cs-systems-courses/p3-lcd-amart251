1afabf40b27f0fbafb7f41d8481401e850c1e58f
amart251 <amartinez251@miners.utep.edu>
2023-07-20 13:47:02 -0600

    ballin shortened

--- lcdLib/lcddemo.c
+++ lcdLib/lcddemo.c
@@ -17,12 +17,7 @@ main()
   clearScreen(COLOR_BLUE);
 
   drawString5x7(80,80, "I put the new Forgis on the Jeep
-I trap until the bloody bottoms is underneath
-Cause all my niggas got it out the streets
-I keep a hundred racks inside my jeans
-I remember hittin the mall with the whole team
-Now a nigga cant answer calls cause Im ballin
-I was wakin up gettin racks in the mornin", COLOR_GREEN, COLOR_RED);
+I trap until the bloody bottoms is underneath", COLOR_GREEN, COLOR_RED);
 
   fillRectangle(30,30, 60, 60, COLOR_ORANGE);
   
