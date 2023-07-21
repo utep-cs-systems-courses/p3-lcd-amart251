a932fc97a3ce81399df5e1a7f7691fe70d940417
amart251 <amartinez251@miners.utep.edu>
2023-07-21 12:16:51 -0600

    x=10,y=10

--- lcdLib/lcddemo.c
+++ lcdLib/lcddemo.c
@@ -39,6 +39,6 @@ main()
   drawString5x7(0, 180, "ttin' racks in the", COLOR_GREEN, COLOR_RED);
   drawString5x7(0, 190, "mornin'", COLOR_GREEN, COLOR_RED);*/
 
-  fillRectangle(0,0, 60, 60, COLOR_SEA_GREEN);
+  fillRectangle(10,10, 10, 60, COLOR_SEA_GREEN);
   
 }
